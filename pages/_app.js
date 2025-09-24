f5889a2301275c215e749b6a79d39ee48f91a9c5
yxbudgqjdqdxq <93042363+yxbudgqjdqdxq@users.noreply.github.com>
2025-09-24 06:15:57 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
 
-import '../styles/globals.css'
-import AnimatedBackground from '../components/AnimatedBackground'
+import '@/styles/globals.css'
+import AnimatedBackground from '@/components/AnimatedBackground'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <AnimatedBackground />
-      <div className="site-root">
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </>
   )
 }
