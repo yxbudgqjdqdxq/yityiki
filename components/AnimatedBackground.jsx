744446de5ade0045ef07faa5a10eeee9a92155b9
yxbudgqjdqdxq <93042363+yxbudgqjdqdxq@users.noreply.github.com>
2025-09-24 06:14:14 -0700

    Update AnimatedBackground.jsx

--- components/AnimatedBackground.jsx
+++ components/AnimatedBackground.jsx
@@ -1,11 +1,12 @@
 
+// components/AnimatedBackground.js
 export default function AnimatedBackground() {
   return (
-    <div className="animated-bg" aria-hidden>
-      <div className="blob blob1" />
-      <div className="blob blob2" />
-      <div className="blob blob3" />
-      <div className="bg-noise" />
+    <div className="animated-bg">
+      <div className="blob blob1"></div>
+      <div className="blob blob2"></div>
+      <div className="blob blob3"></div>
+      <div className="blob blob4"></div>
     </div>
-  )
+  );
 }
