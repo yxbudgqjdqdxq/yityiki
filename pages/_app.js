534bbb0d9f645cabcc8dac43bd21d28b5a596877
yxbudgqjdqdxq <93042363+yxbudgqjdqdxq@users.noreply.github.com>
2025-09-24 06:32:42 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 
 import '@/styles/globals.css'
 import AnimatedBackground from '@/components/AnimatedBackground'
+import BackgroundMusic from '@/components/BackgroundMusic'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <AnimatedBackground />
+      <BackgroundMusic />
       <Component {...pageProps} />
     </>
   )
